29c68b75f252744a58924b4be83c764a89767d6d
sh1nraq <itzeclips3off@gmail.com>
2025-04-20 16:06:57 +0300

    Update config.py

--- config.py
+++ config.py
@@ -4,4 +4,4 @@ class Config:
 
     PROJECT_ROOT = Path(__file__).parent
     MODEL_PATH = PROJECT_ROOT / 'models' / 'best.pt'
-    VIDEO_SOURCE = PROJECT_ROOT / 'data' / 'police_car_fire_ccvt.mp4'
+    VIDEO_SOURCE = PROJECT_ROOT / 'data' / 'gen_fire.mp4'
